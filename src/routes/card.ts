7ee69099b687d2475993f0f70d4db6694d0eb629
bersiroth <caronbernard1@gmail.com>
2020-08-23 19:39:36 +0200

    add cards endpoint (#20)

---
+++ src/routes/card.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import {Request, Response} from 'express';
+import CardsController from '../controllers/card';
+import authCheck from "../middlewares/authCheck";
+
+const router = express.Router();
+const cardsController = new CardsController();
+
+router
+    .use('/v1/cards', authCheck)
+    .get('/v1/cards/:code',(req: Request, res: Response) => cardsController.get(req, res))
+    .get('/v1/cards/',(req: Request, res: Response) => cardsController.getAll(req, res));
+
+export default router;
---
+++ test/controllers/cards.ts
@@ -0,0 +1,63 @@
+import chai from 'chai';
+import app from '../../src/app';
+import chaiHttp = require("chai-http");
+import {getRepository} from "typeorm";
+import User from "../../src/entities/user";
+import JWT from "jsonwebtoken";
+import Card from "../../src/entities/card";
+
+chai.use(chaiHttp);
+const {expect, request} = chai;
+const authorizationHeader = 'bearer ' + JWT.sign({iss: 'https://accounts.google.com'}, 'test');
+
+describe('Controller cards', async(): Promise<void> => {
+
+    let server: ChaiHttp.Agent;
+
+    before(async(): Promise<void> => {
+        const startedApp = await app;
+        server = request(startedApp).keepOpen();
+    });
+
+    after(async(): Promise<void> => {
+        server.close();
+    });
+
+    describe('GET /cards', async (): Promise<void> => {
+        it('should return all card', async(): Promise<void> => {
+            const databaseCard =  await getRepository(Card).find();
+            await server.get('/api/v1/cards').set('authorization', authorizationHeader).then((res): void => {
+                expect(res.error).to.be.false;
+                expect(res).to.have.status(200);
+                expect(res.body.total).to.be.equal(databaseCard!.length);
+                const card = res.body.cards[0];
+                expect(card.code).to.be.equal(databaseCard![0].code);
+                expect(card.rarity).to.be.equal(databaseCard![0].rarity);
+                expect(card.name).to.be.equal(databaseCard![0].name);
+                expect(card.text).to.be.equal(databaseCard![0].text);
+            });
+        });
+    });
+
+    describe('GET /cards/{code}', async (): Promise<void> => {
+        it('should return a card', async(): Promise<void> => {
+            const databaseCard =  await getRepository(Card).findOne();
+            await server.get('/api/v1/cards/'+databaseCard!.code).set('authorization', authorizationHeader).then((res): void => {
+                expect(res.error).to.be.false;
+                expect(res).to.have.status(200);
+                const card = res.body;
+                expect(card.code).to.be.equal(databaseCard!.code);
+                expect(card.rarity).to.be.equal(databaseCard!.rarity);
+                expect(card.name).to.be.equal(databaseCard!.name);
+                expect(card.text).to.be.equal(databaseCard!.text);
+            });
+        });
+
+        it('card not exist', async(): Promise<void> => {
+            await server.get('/api/v1/cards/codeDontExist').set('authorization', authorizationHeader).then((res): void => {
+                expect(res).to.have.status(404);
+                expect(res.body.message).to.be.equal('card not found');
+            });
+        });
+    });
+});
