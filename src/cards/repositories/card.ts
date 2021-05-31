2c11dac77a290383582dbd0bc6aa3b9d755dd1e0
bersiroth <caronbernard1@gmail.com>
2021-05-31 11:35:34 +0200

    feat/cards-filter

--- src/cards/repositories/card.ts
+++ src/cards/repositories/card.ts
@@ -107,11 +107,21 @@ export class CardRepository extends Repository<Card> {
         if (filter.categories) {
             cardsQuery.andWhere(
                 new Brackets((qb) => {
-                    qb.where('"c"."category1" IN (:...categories)', {
-                        categories: filter.categories,
-                    }).orWhere('"c"."category2" IN (:...categories)', {
-                        categories: filter.categories,
-                    });
+                    qb.where(
+                        '"c"."category1" ILIKE ANY(ARRAY[:...categories])',
+                        {
+                            categories: filter.categories?.map(
+                                (value) => `%${value}%`
+                            ),
+                        }
+                    ).orWhere(
+                        '"c"."category2" ILIKE ANY(ARRAY[:...categories])',
+                        {
+                            categories: filter.categories?.map(
+                                (value) => `%${value}%`
+                            ),
+                        }
+                    );
                 })
             );
         }
