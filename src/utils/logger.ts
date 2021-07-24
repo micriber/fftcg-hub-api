905548d1bb5d57d00a75db05dd38c6261b6ea5dc
pawndev <coquelet.c@gmail.com>
2021-07-24 13:06:09 +0200

    fix(logger): Remove file transporter and disable color in production

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,47 +1,33 @@
 import winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
 
 const format = winston.format;
 const env = process.env.NODE_ENV ?? 'development';
 
-const logger = winston.createLogger({
-    transports: new DailyRotateFile({
-        filename: `logs/${env}/api-%DATE%.log`,
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        format: format.combine(
-            format.timestamp(),
-            format.printf((info) => {
-                const body = info.body
-                    ? `body : ${JSON.stringify(info.body)}`
-                    : '';
-                const params = info.params
-                    ? `params : ${JSON.stringify(info.params)}`
-                    : '';
-                return `${info.timestamp as string} ${info.level}: ${
-                    info.message
-                } ${body} ${params}`;
-            })
-        ),
-    }),
-});
+const devFormat = format.combine(
+    format.timestamp(),
+    format.colorize(),
+    format.printf(
+        (info) =>
+            `${info.timestamp as string} ${info.level}: ${
+                info.message
+            }`
+    )
+);
 
-/* istanbul ignore next */
-if (process.env.NODE_ENV !== 'test') {
-    logger.add(
-        new winston.transports.Console({
-            format: format.combine(
-                format.timestamp(),
-                format.colorize(),
-                format.printf(
-                    (info) =>
-                        `${info.timestamp as string} ${info.level}: ${
-                            info.message
-                        }`
-                )
-            ),
-        })
-    );
-}
+const prodFormat = format.combine(
+    format.timestamp(),
+    format.printf(
+        (info) =>
+            `${info.timestamp as string} ${info.level}: ${
+                info.message
+            }`
+    )
+);
+
+const logger = winston.createLogger({
+  transports: new winston.transports.Console({
+    format: env === 'production' ? prodFormat : devFormat
+  })
+});
 
 export default logger;
