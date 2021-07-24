f1e6ba572c092b7cd08d00837f282edf15e21c4f
pawndev <coquelet.c@gmail.com>
2021-07-24 13:18:07 +0200

    fix(logger): Lint

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,27 +7,21 @@ const devFormat = format.combine(
     format.timestamp(),
     format.colorize(),
     format.printf(
-        (info) =>
-            `${info.timestamp as string} ${info.level}: ${
-                info.message
-            }`
+        (info) => `${info.timestamp as string} ${info.level}: ${info.message}`
     )
 );
 
 const prodFormat = format.combine(
     format.timestamp(),
     format.printf(
-        (info) =>
-            `${info.timestamp as string} ${info.level}: ${
-                info.message
-            }`
+        (info) => `${info.timestamp as string} ${info.level}: ${info.message}`
     )
 );
 
 const logger = winston.createLogger({
-  transports: new winston.transports.Console({
-    format: env === 'production' ? prodFormat : devFormat
-  })
+    transports: new winston.transports.Console({
+        format: env === 'production' ? prodFormat : devFormat,
+    }),
 });
 
 export default logger;
